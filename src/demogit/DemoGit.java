cdb59f467ea5d77fafb2b7e2ae8aa770f205e8e2
radhouanet <radhouanet@172.29.102.215>
2016-09-26 13:41:03 -0400

    Creation de classe

--- src/demogit/DemoGit.java
+++ src/demogit/DemoGit.java
@@ -15,8 +15,7 @@ public class DemoGit {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Hello World!");
-        System.out.println("Bonjour");
+        HelloClass.hello();
     }
     
 }
