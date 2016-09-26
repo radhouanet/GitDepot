537a766a78b06fb78946edf139198b5b1c2701a3
radhouanet <radhouanet@172.29.102.215>
2016-09-26 16:01:32 -0400

    j'ai dit bonjour ange

---
+++ src/demogit/HelloClass.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package demogit;
+
+/**
+ *
+ * @author radhouanet
+ */
+public class HelloClass {
+    public static void hello(){
+        System.out.println("Hello World!");
+        System.out.println("Bonjour");
+        System.out.println("Bonjour Ange");
+    }
+}
